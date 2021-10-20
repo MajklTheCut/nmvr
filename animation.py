350802f54ae7157ea72294442b4e1210f8f49272
Michal Porezany <michal.porezan@gmail.com>
2021-10-20 15:07:05 +0200

    1st commit

---
+++ animation.py
@@ -0,0 +1,38 @@
+#%%
+
+import numpy as np
+import pandas as pd
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.colors as mcolors
+import matplotlib.collections as mcoll
+
+
+fig, ax = plt.subplots()
+cmap = mcolors.ListedColormap(["white", "red" , "black"])
+bounds = [-0.5, 0.5, 1.5]
+norm = mcolors.BoundaryNorm(bounds, cmap.N)
+map_space = genfromtxt("map.csv", delimiter=";")
+im = ax.imshow(map_space, cmap=cmap, norm=norm)
+
+grid = np.arange(-0.5, 51, 1)
+xmin, xmax, ymin, ymax = -0.5, 50.5, -0.5, 50.5
+lines = ([[(x, y) for y in (ymin, ymax)] for x in grid]
+         + [[(x, y) for x in (xmin, xmax)] for y in grid])
+grid = mcoll.LineCollection(lines, linestyles="solid", linewidths=0.3, color="gray")
+ax.add_collection(grid)
+
+def animate(xCorr, yCorr):
+    data = genfromtxt("map.csv", delimiter=";")
+    data[xCorr, yCorr] = 0.5
+    im.set_data(data)
+    # return a list of the artists that need to be redrawn
+    return [im, grid]
+
+anim = animation.FuncAnimation(fig, animate(2,2), frames=2, interval=1, blit=True, repeat=True)
+plt.show()
+
+
+# %%
+# %%
