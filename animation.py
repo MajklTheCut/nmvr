8546ae715cf58579f20be3d85488909a35d131c2
Michal Porezaný <mchal.porezan@gmail.com>
2021-10-20 19:41:42 +0200

    removed tkinter bullshit, used PYsimpleGUI

--- animation.py
+++ animation.py
@@ -1,52 +1,63 @@
-import tkinter as tk
+
+import matplotlib
 import numpy as np
 import pandas as pd
-import matplotlib
 from numpy import genfromtxt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.colors as mcolors
 import matplotlib.collections as mcoll
+import PySimpleGUI as sg
 
-window = tk.Tk()
-window.title("Sim GUI")
-operator= ""
-
-canvas = tk.Canvas(window, height= 600, width=600, bg="White")
-canvas.pack()
-
-frame = tk.Frame(window, bg="Light Blue")
-frame.place(relwidth=0.9, relheight=0.9, relx=0.05, rely=0.05)
+matplotlib.use("TkAgg")
 
 
 fig, ax = plt.subplots()
-cmap = mcolors.ListedColormap(["white", "black", "lime",])
+cmap = mcolors.ListedColormap(["gray", "black", "lime",])
 bounds = [-0.5, 0.5, 1.5, 2.5]
 norm = mcolors.BoundaryNorm(bounds, cmap.N)
 
-map_space = genfromtxt("map.csv", delimiter=";")
+map_space = genfromtxt("/home/nvmr/Documents/nmvr/nmvr/map.csv", delimiter=";")
 im = ax.imshow(map_space, cmap=cmap, norm=norm)
+
+def moveRobot(xCoor, yCoor):
+    map_space[xCoor,yCoor] = 2
+
+moveRobot(2,2)
 
 grid = np.arange(-0.5, 51, 1)
 xmin, xmax, ymin, ymax = -0.5, 50.5, -0.5, 50.5
 lines = ([[(x, y) for y in (ymin, ymax)] for x in grid]
          + [[(x, y) for x in (xmin, xmax)] for y in grid])
-grid = mcoll.LineCollection(lines, linestyles="solid", linewidths=0.3, color="gray")
+grid = mcoll.LineCollection(lines, linestyles="solid", linewidths=0.3, color="white")
 ax.add_collection(grid)
 
+
 def animate(i):
-    data = genfromtxt("map.csv", delimiter=";")
+    data = genfromtxt("/home/nvmr/Documents/nmvr/nmvr/map.csv", delimiter=";")
     data[5,5] = 2
     im.set_data(data)
-    # return a list of the artists that need to be redrawn
     return [im, grid]
 
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=1)
+    return figure_canvas_agg
+
+layout= [[sg.Text("Basic simulator GUI")],
+        [sg.Canvas(key="-CANVAS-")],
+        [sg.Button("OK")]]
+
+window = sg.Window("Sim GUI", layout, finalize=True, element_justification="center")
+
+fig_canvas_agg = draw_figure(window["-CANVAS-"].TKCanvas, fig)
+
+event, values = window.read()
 
 anim = animation.FuncAnimation(fig, animate, frames=2, interval=1, blit=True, repeat=True)
 
-canvas = FigureCanvasTkAgg(fig, master=window)
-canvas.draw()
-canvas.get_tk_widget().grid(column=0, row=1)
+window.close
 
-tk.mainloop()
+
